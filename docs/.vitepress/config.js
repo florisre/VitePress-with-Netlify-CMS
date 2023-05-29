fb945bf48373d147242d392215412bce35f1232a
Floris Remmert <floris.remmert@posteo.de>
2023-05-29 10:56:21 +0200

    neuer Ansatz mit Loesung von anderswo angepasst

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,54 +1,42 @@
-const fs = require('fs');
-const path = require('path');
+const { defineUserConfig } = require("@vuepress/cli");
+const { path, glob } = require("@vuepress/utils");
 
-module.exports = {
-  title: 'StuRa',
-  description: 'Willkommen auf der offiziellen Seite Deiner Studierendenvertretung',
+let protokollFiles = glob
+  .sync("docs/protokolle/**/*.md")
+  .map((f) => f.replace("docs", "").replace("index.md", ""));
+
+const config = defineUserConfig({
+  lang: "en-US",
+  title: "StuRa",
+  description: "Willkommen auf der offiziellen Seite Deiner Studierendenvertretung",
   themeConfig: {
-    repo: 'florisre/VitePress-with-Netlify-CMS',
-    docsDir: 'docs',
+    repo: "florisre/VitePress-with-Netlify-CMS",
+    docsDir: "docs",
     editLinks: true,
-    editLinkText: 'Edit this page on GitHub',
-    lastUpdated: 'Last Updated',
+    editLinkText: "Edit this page on GitHub",
+    lastUpdated: "Last Updated",
     nav: [
       {
-        text: 'Sitzungen',
-        link: '/sitzungen/',
+        text: "Sitzungen",
+        link: "/sitzungen/",
       },
       {
-        text: 'Protokolle',
-        link: '/protokolle/',
+        text: "Protokolle",
+        link: "/protokolle/",
       },
       {
-        text: 'Instagram',
-        link: 'https://www.instagram.com/sturaunifreiburg/',
+        text: "Instagram",
+        link: "https://www.instagram.com/sturaunifreiburg/",
       },
     ],
-    sidebar: getProtokolleSidebar(),
+    sidebar: [
+      {
+        text: "Protokolle",
+        children: protokollFiles,
+      },
+    ],
   },
-};
+});
 
-function getProtokolleSidebar() {
-  const basePath = '/docs/protokolle'; // Update the base path to '/docs/protokolle'
-  const sidebar = [];
-
-  const subdirectories = fs.readdirSync(path.resolve(__dirname, `.${basePath}`))
-    .filter((file) => fs.statSync(path.join(__dirname, `.${basePath}`, file)).isDirectory());
-
-  subdirectories.forEach((subdirectory) => {
-    const subdirectoryPath = path.join(basePath, subdirectory);
-    const files = fs.readdirSync(path.resolve(__dirname, `.${subdirectoryPath}`))
-      .filter((file) => file.endsWith('.md'))
-      .map((file) => `${subdirectoryPath}/${file}`);
-
-    if (files.length > 0) {
-      sidebar.push({
-        text: subdirectory,
-        children: files,
-      });
-    }
-  });
-
-  return sidebar;
-}
+module.exports = config;
 
