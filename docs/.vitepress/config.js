54b2e70d887ae4d135c8925e0e5eda0463ba66e4
Floris Remmert <floris.remmert@posteo.de>
2023-05-29 11:26:39 +0200

    ok last try

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -55,21 +55,6 @@ export default {
   },
   alias: {
     '@theme/HomeFooter.vue': resolve(__dirname, '../components/MyHomeFooter.vue'),
-  },
-  configureServer: [
-    (app) => {
-      app.use((req, res, next) => {
-        // Serve the `/admin` directory as static files
-        if (req.originalUrl.startsWith('/admin')) {
-          const adminPath = resolve(__dirname, '../public/admin');
-          if (fs.existsSync(adminPath)) {
-            express.static(adminPath)(req, res, next);
-            return;
-          }
-        }
-        next();
-      });
-    },
-  ],
+  }
 };
 
