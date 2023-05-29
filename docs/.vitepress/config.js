ac6809b8ec0336931183fd652ae4b0a9a418e9b9
Floris Remmert <floris.remmert@posteo.de>
2023-05-29 11:24:55 +0200

    added correct path and corrected function with failsafe I might not want

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,10 @@
 import { resolve } from 'path';
+import { fileURLToPath } from 'url';
 import { fs } from 'fs';
 import glob from 'glob';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = resolve(__filename, '../');
 
 const protokollFiles = glob
   .sync('protokolle/**/*.md', { cwd: __dirname })
@@ -52,5 +56,20 @@ export default {
   alias: {
     '@theme/HomeFooter.vue': resolve(__dirname, '../components/MyHomeFooter.vue'),
   },
+  configureServer: [
+    (app) => {
+      app.use((req, res, next) => {
+        // Serve the `/admin` directory as static files
+        if (req.originalUrl.startsWith('/admin')) {
+          const adminPath = resolve(__dirname, '../public/admin');
+          if (fs.existsSync(adminPath)) {
+            express.static(adminPath)(req, res, next);
+            return;
+          }
+        }
+        next();
+      });
+    },
+  ],
 };
 
